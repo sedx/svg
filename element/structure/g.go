604ee6dd465997fd43671ec194efdb3a8ef0fa4f
Ilia Bondarenko <bondarenko.ik@gmail.com>
2018-03-26 16:11:04 +0500

    G is animataed now

--- element/structure/g.go
+++ element/structure/g.go
@@ -3,11 +3,14 @@ package structure
 import (
 	"encoding/xml"
 
+	"github.com/sedx/svg/element/animation"
+
 	"github.com/sedx/svg/element"
 )
 
 type G struct {
 	element.SVGElement
 	element.SVGContainer
+	animation.Animated
 	XMLName xml.Name `xml:"g"`
 }
