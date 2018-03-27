4f61156fdc305e5e940cf886899aa440b32d8665
Ilia Bondarenko <bondarenko.ik@gmail.com>
2018-03-27 15:25:28 +0500

    G has transform attribute now

--- element/structure/g.go
+++ element/structure/g.go
@@ -3,6 +3,8 @@ package structure
 import (
 	"encoding/xml"
 
+	"github.com/sedx/svg/attributes"
+
 	"github.com/sedx/svg/element/animation"
 
 	"github.com/sedx/svg/element"
@@ -12,5 +14,6 @@ type G struct {
 	element.SVGElement
 	element.SVGContainer
 	animation.Animated
+	attributes.Transformable
 	XMLName xml.Name `xml:"g"`
 }
