c43152ee3126b8f0569c14836e477f8fe3a8c071
Ilia Bondarenko <bondarenko.ik@gmail.com>
2018-03-27 15:31:36 +0500

    Add mask element

---
+++ element/mask/mask.go
@@ -0,0 +1,30 @@
+package mask
+
+import (
+	"encoding/xml"
+	"fmt"
+
+	"github.com/sedx/svg/attributes"
+	"github.com/sedx/svg/element"
+)
+
+type Mask struct {
+	element.SVGContainer
+	element.SVGElement
+	attributes.Positioned
+	attributes.Measured
+	// todo enum
+	MaskContentUnits string   `xml:"maskContentUnits,attr,omitempty"`
+	MaskUnits        string   `xml:"maskUnits,attr,omitempty"`
+	XMLName          xml.Name `xml:"mask"`
+}
+
+func (m *Mask) MarshalXMLAttr(name xml.Name) (xml.Attr, error) {
+	if m == nil {
+		return xml.Attr{}, nil
+	}
+	if m.ID == "" {
+		return xml.Attr{}, fmt.Errorf("ID is missing for %s", name)
+	}
+	return xml.Attr{Name: name, Value: fmt.Sprintf("url(#%s)", m.ID)}, nil
+}
