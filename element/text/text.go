75003b3e773816c11a22d36b033e8ac6a266f1ca
Ilia Bondarenko <bondarenko.ik@gmail.com>
2018-03-28 14:20:09 +0500

    Update text attributes
    Add Filled attributes.
    Add Dx and Dy

--- element/text/text.go
+++ element/text/text.go
@@ -5,12 +5,16 @@ import (
 
 	"github.com/sedx/svg/attributes"
 	"github.com/sedx/svg/element"
+	"github.com/sedx/svg/types"
 )
 
 // Text svg element
 type Text struct {
 	element.SVGElement
 	attributes.Positioned
-	Content string   `xml:",chardata"`
-	XMLName xml.Name `xml:"text"`
+	attributes.Filled
+	Dx      types.Length `xml:"dx,attr,omitempty"`
+	Dy      types.Length `xml:"dy,attr,omitempty"`
+	Content string       `xml:",chardata"`
+	XMLName xml.Name     `xml:"text"`
 }
